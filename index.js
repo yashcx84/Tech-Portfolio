5f632997112736931876dc463e10072ee7d2cb1a
yashcx84 <yashcx84@gmail.com>
2024-02-04 21:08:46 +0530

    Final Modified.

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ let output = document.getElementById("date-obj");
 
 let mydate = new Date();
 
-output.innerHTML = "@" + mydate.getFullYear()+" Built";
+output.innerHTML = "Buildx"+"@" + mydate.getFullYear();
+
+// --
+let ham = document.getElementById("ham");
+let hamwindow = document.getElementById("ham-window");
+
+ham.addEventListener('click', () => {
+    hamwindow.classList.toggle("window-open");
+});
